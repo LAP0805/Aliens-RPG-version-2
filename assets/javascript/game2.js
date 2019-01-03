1f360bc7524660503e59182c13efef55676b01b5
Janxxy <lesley.pond@gmail.com>
2019-01-03 12:49:50 -0500

    game

---
+++ assets/javascript/game2.js
@@ -0,0 +1,268 @@
+$(document).ready(function() {
+
+    var hero = "";
+    var villain;
+    var getID="";
+    var defeated=[];
+    
+    
+    ripleyStats = {
+        name: "Ripley",
+        hP: 200,
+        attack: 10,
+        increase: 10,
+        counterAttack:7,
+        image: ("assets/images/ripley.jpg"),
+    },
+    vasquesStats = {
+        name: "Vasquez",
+        hP: 170,
+        attack: 5,
+        increase: 5,
+        counterAttack:10,
+        image: ("assets/images/vasquez.jpg"),
+    },
+    hicksStats= {
+        name: "Hicks",
+        hP: 150,
+        attack: 15,
+        increase: 15,
+        counterAttack:10,
+        image: ("assets/images/hicks.jpg"),
+    },
+
+    alienStats = {
+        name: "Xenomorph",
+        hP: 350,
+        attack: 16,
+        increase: 16,
+        counterAttack:12,
+        image: ("assets/images/alien.jpg"),
+    }
+
+///asign hero
+   function assignHero(){
+       var getMe= $("#yourCharacter div");
+    
+        getID = getMe.attr("id");
+     
+       if (getID === "Ripley"){
+        hero=ripleyStats;
+        console.log(hero);
+    }
+    else if (getID==="Vasquez"){
+        hero=vasquesStats;
+        console.log(hero);
+    }
+    else if (getID==="Hicks"){
+        hero=hicksStats;
+        console.log(hero);
+    }
+    else if (getID==="Xenomorph"){
+        hero=alienStats;
+        console.log(hero);
+    }
+   }
+
+//assign villain
+function assignVillain(){
+    var getVillain= $("#currentOpponent div");   
+        getVillainID = getVillain.attr("id");   
+       if (getVillainID === "Ripley"){
+        villain=ripleyStats;  
+        console.log(villain);
+    }
+    else if (getVillainID==="Vasquez"){
+        villain=vasquesStats;  
+        console.log(villain);     
+    }
+    else if (getVillainID==="Hicks"){
+        villain=hicksStats;  
+        console.log(villain);   
+    }
+    else if (getVillainID==="Xenomorph"){
+        villain=alienStats;   
+        console.log(villain);    
+    }   
+}
+
+//create characters. I initially did this in HTML but found a bug at the end of the game and decided to do this//
+function createChar(character){
+ var newDiv = $("<div>");
+ newDiv.attr("id",character.name);
+ newDiv.css({border:"2px solid black", borderRadius:"15px", width:"200px", margin:"5px",float:"left", background:"#ebebeb", fontFamily:"monospace", fontSize:"15px", textAlign:"center"});
+ var firstP = $("<p>");
+ firstP.text(character.name);
+ newDiv.append(firstP);
+ var img = $("<img src = " + character.image + ">");
+ img.css({height:"125px", width:"125px", borderRadius:"15px"});
+ newDiv.append(img);
+ var secondP = $("<p>");
+ secondP.text("health: " + character.hP + " attack: " + character.attack + " counter-attack: " + character.counterAttack);
+ newDiv.append(secondP);
+ $("#characters").append(newDiv);
+};
+   
+ 
+ 
+ 
+ createChar(ripleyStats);
+ createChar(vasquesStats);
+ createChar(hicksStats);
+ createChar(alienStats);
+
+ $("#characters div").on("click", function(){
+    $("#yourCharacter").append(this);
+   $("#enemies").append($("#characters div").not(this));
+   $("#enemies div").css({background:"red"});
+   assignHero();
+   
+   
+   $("#enemies div").on("click", function(){
+    if($("#currentOpponent").is(":empty")){
+    $("#currentOpponent").append($(this));
+    assignVillain();
+    }
+    else {
+        $("#enemies").append(this);
+    }
+
+});
+});
+
+
+//on attack button click//
+$("#attack").on("click", function(){
+    if ($("#currentOpponent").is(":empty")){
+         $("#gameInfo2").text("Pick an opponent");
+      return setTimeout(function(){ $("#gameInfo2").text("")}, 2000);
+
+    }
+    else{
+        villain.hP = villain.hP - hero.attack;
+        hero.hP = hero.hP - villain.counterAttack;
+        $("#gameInfo2").text("You attacked " + villain.name + " for " + hero.attack + ". " + villain.name + " counter-attacked for " + villain.counterAttack);
+        hero.attack = hero.attack + hero.increase;
+        if (hero.hP < 1){
+            $("#restart").css({display:"block"});
+            $("#gameInfo2").text("You lose!");
+        }
+        else if(villain.hP <1){
+            defeated.push(villain);
+            isGameWon();   
+        }
+    }
+    });
+
+//restart game on button click
+    $("#restart").on("click", function(){
+        location.reload();
+    });
+
+//new round//
+function newRound(){
+    assignVillain();    
+}
+
+//is Game won//
+function isGameWon(){
+    if (defeated.length >2){
+        $("#gameInfo2").text("You won the whole game!");
+        $("#restart").css({display:"block"});
+        return
+    }
+    else{
+        $("#gameInfo2").text("You won the round! Select a new opponent.");
+        setTimeout(function(){ $("#gameInfo2").text("")}, 2000);
+         $("#currentOpponent div").remove();
+         console.log(defeated.length);
+         villain = "";
+         newRound();
+    }
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
